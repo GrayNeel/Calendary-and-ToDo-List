4102902a796bc489a883fadd5f3fe81069cddd24
GrayNeel <linoss18@gmail.com>
2021-10-21 20:03:45 +0200

    added DB to CalDAV

--- project/server/server.php
+++ project/server/server.php
@@ -1,30 +1,81 @@
 <?php
 
-use
-    Sabre\DAV;
+/*
 
-// The autoloader
-require 'vendor/autoload.php';
+CalendarServer example
 
-// Now we're creating a whole bunch of objects
-$rootDirectory = new DAV\FS\Directory('public');
+This server features CalDAV support
 
-// The server object is responsible for making sense out of the WebDAV protocol
-$server = new DAV\Server($rootDirectory);
+*/
 
-// If your server is not on your webroot, make sure the following line has the
-// correct information
-$server->setBaseUri('/');
+// settings
+date_default_timezone_set('Canada/Eastern');
 
-// The lock manager is reponsible for making sure users don't overwrite
-// each others changes.
-$lockBackend = new DAV\Locks\Backend\File('data/locks');
-$lockPlugin = new DAV\Locks\Plugin($lockBackend);
-$server->addPlugin($lockPlugin);
+// If you want to run the SabreDAV server in a custom location (using mod_rewrite for instance)
+// You can override the baseUri here.
+$baseUri = '/';
 
-// This ensures that we get a pretty index in the browser, but it is
-// optional.
-$server->addPlugin(new DAV\Browser\Plugin());
+/* Database */
+$pdo = new PDO('sqlite:data/db.sqlite');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// All we need to do now, is to fire up the server
+//Mapping PHP errors to exceptions
+function exception_error_handler($errno, $errstr, $errfile, $errline) {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
+
+// Files we need
+require_once 'vendor/autoload.php';
+
+// Backends
+$authBackend = new Sabre\DAV\Auth\Backend\PDO($pdo);
+$calendarBackend = new Sabre\CalDAV\Backend\PDO($pdo);
+$principalBackend = new Sabre\DAVACL\PrincipalBackend\PDO($pdo);
+
+// Directory structure
+$tree = [
+    new Sabre\CalDAV\Principal\Collection($principalBackend),
+    new Sabre\CalDAV\CalendarRoot($principalBackend, $calendarBackend),
+];
+
+$server = new Sabre\DAV\Server($tree);
+
+if (isset($baseUri))
+    $server->setBaseUri($baseUri);
+
+/* Server Plugins */
+$authPlugin = new Sabre\DAV\Auth\Plugin($authBackend);
+$server->addPlugin($authPlugin);
+$authBackend->setRealm('CalendarioPDS');
+
+$aclPlugin = new Sabre\DAVACL\Plugin();
+$server->addPlugin($aclPlugin);
+
+/* CalDAV support */
+$caldavPlugin = new Sabre\CalDAV\Plugin();
+$server->addPlugin($caldavPlugin);
+
+/* Calendar subscription support */
+$server->addPlugin(
+    new Sabre\CalDAV\Subscriptions\Plugin()
+);
+
+/* Calendar scheduling support */
+$server->addPlugin(
+    new Sabre\CalDAV\Schedule\Plugin()
+);
+
+/* WebDAV-Sync plugin */
+$server->addPlugin(new Sabre\DAV\Sync\Plugin());
+
+/* CalDAV Sharing support */
+$server->addPlugin(new Sabre\DAV\Sharing\Plugin());
+$server->addPlugin(new Sabre\CalDAV\SharingPlugin());
+
+// Support for html frontend
+$browser = new Sabre\DAV\Browser\Plugin();
+$server->addPlugin($browser);
+
+// And off we go!
 $server->exec();
