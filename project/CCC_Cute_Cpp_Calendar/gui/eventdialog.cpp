f6d4becb33dd30d087ccdeef54e343441e038cf2
lelexdrugo <gabrielescaffidi@gmail.com>
2021-12-02 18:13:14 +0100

    feat: added check on DateTime field of eventDialog

--- project/CCC_Cute_Cpp_Calendar/gui/eventdialog.cpp
+++ project/CCC_Cute_Cpp_Calendar/gui/eventdialog.cpp
@@ -21,10 +21,27 @@ void EventDialog::on_confirmButton_clicked()
     // Not implemented yet
     // QString _rrule;
 
-    QDateTime startDateTime;
-    QDateTime endDateTime;
+    //Se usiamo questi widget dobbiamo inserire dei check sulle ore o quantomeno usare un widget di tipo TimeEdit
+    //Oppure si sceglie il widget che genera automaticamente oggetti QDateTime
+//    QDate startDate = ui->calendarWidget->selectedDate();
+//    QDate endDate= ui->calendarWidget_2->selectedDate();
+//    //h must be in the range 0 to 23, m and s must be in the range 0 to 59, and ms must be in the range 0 to 999.
+//    //Insert check on the field
+//    QTime startTime= QTime(ui->startingHoursLine->text().toInt(), ui->startingMinutesLine->text().toInt());
+//    QTime endTime= QTime(ui->endingHoursLine->text().toInt(), ui->endingMinutesLine->text().toInt());
+//    QDateTime startDateTime = QDateTime(startDate, startTime);
+//    QDateTime endDateTime = QDateTime(endDate, endTime);
 
-    emit eventAddEvent(summary, location, description, startDateTime, endDateTime);
+    //Inserire check sulle date (seconda successiva alla prima)
+    QDateTime startDateTime = ui->dateTimeEditStarting->dateTime();
+    QDateTime endDateTime = ui->dateTimeEditEnding->dateTime();
+
+    if(startDateTime>=endDateTime)
+         ui->errorMessageLabel->setText(QString("La data di fine deve essere successiva a quella di inizio"));
+    else {
+         emit eventAddEvent(summary, location, description, startDateTime, endDateTime);
+    }
+
 }
 
 void EventDialog::setCal(Calendar *newCal)
